5fee060b3887e7a6abc640cf3a0517474e029a84
Kabir Tamari <kabirtamari@gmail.com>
2020-02-10 21:38:14 +0545

    added file saver, and some quality of life changes

--- webbot_jere.py
+++ webbot_jere.py
@@ -1,19 +1,19 @@
 from selenium import webdriver
-# from bs4 import BeautifulSoup
+import json 
+
 URL = "https://aava.site-ym.com/searchserver/people.aspx?id=88D26135-6899-4AB1-998E-455D0D07FEF3&cdbid=&canconnect=0&canmessage=0&map=True&toggle=False&hhSearchTerms="
-
-temp = 'https://aava.site-ym.com/members/?id=61051743'
-
 
 
 def get_links(driver):
+    member_links = []
     list_of_docs = driver.find_elements_by_class_name('lineitem')
     for doc in list_of_docs:
         try:
             link_element = (doc.find_element_by_tag_name('a'))
-            print(link_element.get_attribute('href'))
+            member_links.append(link_element.get_attribute('href'))
         except:
             pass
+    return(member_links)
     
 
 def next_page(driver):
@@ -25,98 +25,127 @@ def next_page(driver):
 
 
 
-def mine_data(driver, website):
-    driver.get(website)
-    employee_json = {
-        'first_name':'',
-        'last_name':'',
-        'full_name':'',
-        'title':'',
-        'email':'',
-        'website':'',
-        'address':'',
-        'city':'',
-        'state':'',
-        'country':'',
-        'zip':'',
-        'phone':''
-    }   
-    employee_json['website'] = website
-    texts = driver.find_element_by_class_name('big').text
-    texts = str(texts).split(',')[0]
-    employee_json['full_name'] = str(texts)
-    texts = str(texts).split(' ')
-    if texts[0].__contains__('.'):
-        employee_json['title'] = str(texts[0]).strip()
-        employee_json['first_name'] = (texts[1]).strip()
-    else:
-        employee_json['first_name'] = texts[0].strip()
-    employee_json['last_name'] = texts[len(texts)-1]
-    
-
-    #find email
-    email = ''
+def mine_data(website):
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("headless")
+    # child_driver = webdriver.Chrome('/home/lurayy/chromedriver', chrome_options=options)
+    child_driver = webdriver.Chrome('/home/lurayy/chromedriver')
     try:
-        info_section = driver.find_elements_by_class_name('ViewTable1')
-        for info in info_section:
-            links = info.find_elements_by_tag_name('a')
-            for link in links:
-                if (link.get_attribute('href').__contains__('mailto:')):
-                    email = link.text
-    except:
-        pass
-    employee_json['email'] = str(email)
-    
+        child_driver.get(website)
 
-    #address zip
-    info_section = driver.find_element_by_id('tdEmployerName')
-    texts = str(info_section.text).split('\n')
-    if texts[len(texts)-1].__contains__("[ Map ]"):
-        texts.pop()
-    space = ' '
-    addresss = space.join(texts)
-    employee_json['address'] = addresss
+        employee_json = {
+            'first_name':'',
+            'last_name':'',
+            'full_name':'',
+            'title':'',
+            'email':'',
+            'website':'',
+            'address':'',
+            'city':'',
+            'state':'',
+            'country':'',
+            'zip':'',
+            'phone':''
+        }   
+        employee_json['website'] = website
+        texts = child_driver.find_element_by_class_name('big').text
+        texts = str(texts).split(',')[0]
+        employee_json['full_name'] = str(texts)
+        texts = str(texts).split(' ')
+        if texts[0].__contains__('.'):
+            employee_json['title'] = str(texts[0]).strip()
+            employee_json['first_name'] = (texts[1]).strip()
+        else:
+            employee_json['first_name'] = texts[0].strip()
+        employee_json['last_name'] = texts[len(texts)-1]
+        
 
-    links = info_section.find_elements_by_tag_name('a')
-    for link in links:
-        link_text = (link.get_attribute('href'))
-        if link_text.__contains__('city'):
-            employee_json['city'] = str(link.text).strip()
-        if link_text.__contains__('state'):
-            employee_json['state'] = str(link.text).strip()
-        if link_text.__contains__('country'):
-            employee_json['country'] = str(link.text).strip()
-            for text in texts:
-                if text.__contains__(str(link.text)):
-                    temp = text.split(link.text)
-                    employee_json['zip'] = str(temp[0]).strip()
-
-    #phone
-    try:
-        phone_element = driver.find_element_by_id('tdWorkPhone')
+        #find email
+        email = ''
         try:
-            phone = str(phone_element.text).split('(Phone)')[0]
-            employee_json['phone'] = phone
+            info_section = child_driver.find_elements_by_class_name('ViewTable1')
+            for info in info_section:
+                links = info.find_elements_by_tag_name('a')
+                for link in links:
+                    if (link.get_attribute('href').__contains__('mailto:')):
+                        email = link.text
         except:
-            employee_json['phone'] = str(phone.text)
+            pass
+        employee_json['email'] = str(email)
+        
+
+        #address zip
+        info_section = child_driver.find_element_by_id('tdEmployerName')
+        texts = str(info_section.text).split('\n')
+        if texts[len(texts)-1].__contains__("[ Map ]"):
+            texts.pop()
+        space = ' '
+        addresss = space.join(texts)
+        employee_json['address'] = addresss
+
+        links = info_section.find_elements_by_tag_name('a')
+        for link in links:
+            link_text = (link.get_attribute('href'))
+            if link_text.__contains__('city'):
+                employee_json['city'] = str(link.text).strip()
+            if link_text.__contains__('state'):
+                employee_json['state'] = str(link.text).strip()
+            if link_text.__contains__('country'):
+                employee_json['country'] = str(link.text).strip()
+                for text in texts:
+                    if text.__contains__(str(link.text)):
+                        temp = text.split(link.text)
+                        employee_json['zip'] = str(temp[0]).strip()
+
+        #phone
+        try:
+            phone_element = child_driver.find_element_by_id('tdWorkPhone')
+            try:
+                phone = str(phone_element.text).split('(Phone)')[0]
+                employee_json['phone'] = phone
+            except:
+                employee_json['phone'] = str(phone.text)
+        except:
+            employee_json['phone'] = ''
+            child_driver.quit()
+        return {'data':employee_json, 'status':1}
+            
     except:
-        employee_json['phone'] = ''
-    return employee_json
+        print('error on ',website)
+        child_driver.quit()
+        return {'status':0}
+    finally:
+        child_driver.quit()
         
 
 if __name__ == "__main__":
     
-    options = webdriver.ChromeOptions()
-    options.add_argument("headless")
-    driver = webdriver.Chrome('/home/lurayy/chromedriver', chrome_options=options)
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("headless")
+    # driver = webdriver.Chrome('/home/lurayy/chromedriver', chrome_options=options)
+    driver = webdriver.Chrome('/home/lurayy/chromedriver')
+    driver.get(URL)
 
-
-
-    # get_links
-    data = mine_data(driver, temp)
-    print(data)
-    # next_page(driver)
+    temp_output = []
+    error_list = []
+    member_links = get_links(driver)
+    
+    for member_link in member_links:
+        response_json = mine_data(member_link)
+        if response_json['status']:
+            temp_output.append(response_json['data'])
+        else:
+            error_list.append(member_link)
+        
     driver.close()
+
+    print(temp_output)
+
+    with open('output_json.json', 'w') as output_file:
+        json.dump(temp_output, output_file)
+
+    with open('error_list.json', 'w') as output_file:
+        json.dump(error_list, output_file)
 
 # firstname, lastname, fullname, title,email, website,
 #  address, city, state,country, zip, , phone ,
